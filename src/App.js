73f3a9d0c4fbee33c39bfca1633013aa149363a7
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-07-23 13:32:28 +0600

    setup all necessary thing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from './Home/Home';
+import Navbar from './Navbar/Navbar';
+import Dashboard from './Dashboard/Dashboard';
+import DashboardHome from './Dashboard/DashboardHome/DashboardHome';
+import Students from './Dashboard/Students/Students';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}>
+        <Route path='/dashboard' element={<DashboardHome/>}/>
+  
+        <Route path='student' element={<Students/>}/>
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Dashboard/DashboardSideber.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+const DashboardSideber = ( {children}) => {
+    return (
+        <div class="drawer drawer-mobile bg-base-200">
+            <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+            <div class="drawer-content ">
+                {/*  <!-- Page content here --> */}
+                {children}
+            </div>
+            <div class="drawer-side">
+                <label for="my-drawer-2" class="drawer-overlay"></label>
+                <ul class="menu p-4 overflow-y-auto w-60 bg-blue-600 text-base-content">
+                <h1>Acadomi</h1>
+                    {/*   Sidebar content here */}
+                        <li><Link to="/dashboard/student">Students</Link></li>
+                </ul>
+
+            </div>
+        </div>
+    );
+};
+
+export default DashboardSideber;
